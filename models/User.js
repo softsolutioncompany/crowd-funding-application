bc157dc907edb9dfad1af55895c3876f1005501f
tunjineo <tunjineo@gmail.com>
2021-10-07 00:07:58 +0100

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+	firstname: String,
+	lastname: String,
+	email: String,
+	telephone: String,
+	username: String,
+	password: String
+});
+
+
+module.exports = mongoose.model('User', userSchema);
